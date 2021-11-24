dadb5ec98aa41024267a6f094989feb52f93b76a
Lunarixus <Nathan@lunarixus.dev>
2021-11-24 04:10:15 +0000

    taimen: Drop vndk_package building
    * I already do this in the common tree.
    
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- hentai_taimen.mk
+++ hentai_taimen.mk
@@ -25,9 +25,6 @@ $(call inherit-product, vendor/hentai/build/product/hentai_product.mk)
 $(call inherit-product, device/google/taimen/device.mk)
 $(call inherit-product, vendor/google/taimen/taimen-vendor.mk)
 
-PRODUCT_PACKAGES += \
-    vndk_package
-
 PRODUCT_COPY_FILES += \
     device/google/taimen/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml
